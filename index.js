548b1846b00a0db22964e67094665b4999bce6f2
younha00 <ayeon0@office.skhu.ac.kr>
2025-03-14 12:40:25 +0900

    no-sandbox 수정

--- index.js
+++ index.js
@@ -2,21 +2,15 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 
 async function run() {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
+    const browser = await puppeteer.launch({
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
     await page.goto('https://vibe.naver.com/chart/total');
     const html = await page.content();
     const title = await page.title();
     const text = await page.evaluate(() => {
         return document.querySelector('body').innerText;
-    });
-    // const songs= await page.evaluate(() => 
-    //     Array.from(document.querySelectorAll('.tracklist tbody tr'), (e) => ({
-    //         rank: e.querySelector('.rank span').innerText,
-    //         song: e.querySelector('.song .link_text span').innerText,
-    //         artist: e.querySelector('.artist .link_artist span').innerText,
-    //     }))
-    // );   
+    }); 
     
     const songs = await page.$$eval('.tracklist tbody tr', (elements) => 
         elements.map((e) => ({
@@ -25,7 +19,6 @@ async function run() {
             artist: e.querySelector('.artist .link_artist span').innerText,
         }))
     );
-    // .json 파일로 저장
 
     fs.writeFile('top100.json', JSON.stringify(songs), (err) => {
         if (err) throw err;
