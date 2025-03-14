a1f75de2f0c4cc59aa83d5e459af8318cd2c5501
younha00 <ayeon0@office.skhu.ac.kr>
2025-03-14 12:59:50 +0900

    Page 변수 추가

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ async function run() {
     const browser = await puppeteer.launch({
         args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
+    const page = await browser.newPage();
     await page.goto('https://vibe.naver.com/chart/total');
     const html = await page.content();
     const title = await page.title();
